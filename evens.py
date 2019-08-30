d2218961719cfcf58597957404e7548381d17587
Steven Murray <tstevenm@gmail.com>
2019-08-30 14:24:37 -0500

    Initial Commit

---
+++ evens.py
@@ -0,0 +1,7 @@
+# Given a list of numbers, print each number in the list that is even.
+
+numbers = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+for number in numbers:
+    if number % 2 == 0:
+        print(number)
